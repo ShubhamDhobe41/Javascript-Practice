9f641507a5d7f0bd20227c2c8484571ea29a3069
ShubhamDhobe41 <shubhamdhobe4142@gmail.com>
2024-09-14 22:29:21 +0530

    first commit
    Date Object in Javascript

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-console.log(Math.floor(23.2));//23
-console.log(Math.ceil(23.2));//24
-console.log(34 ** 2); //1156
-console.log(Math.pow(2,6));//64
-console.log(Math.round(3442.2));//3442
-console.log(Math.round(3442.6));//3443
-console.log(Math.random());//0.7136114441368251
+let myDate = new Date()
+// console.log(myDate);//2024-08-15T15:39:48.161Z
+
+// console.log(myDate.toString());//Thu Aug 15 2024 21:10:32 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString());//Thu Aug 15 2024
+// console.log(myDate.toTimeString());//21:11:58 GMT+0530 (India Standard Time)
+// console.log(myDate.toLocaleDateString());// 8/15/2024
+// console.log(myDate.toISOString());//  2024-08-15T15:41:58.131Z
+// console.log(myDate.toLocaleString());//8/15/2024, 9:13:56 PM
+// console.log(typeof myDate); // Object 
 
 
-console.log(Math.floor(Math.random()*20));//16
-console.log(7/0);//infinity
-console.log(-7/0);//-infinity
-console.log(typeof Infinity);//number
-console.log(0/0);//NaN
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString()); // Mon Jan 23 2023
+let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toLocaleString());//    1/23/2023, 12:00:00 AM
 
-// Input from user 
+// let myDateFormat = new Date("2023-01-14");//YYYY-MM-DD
+let myDateFormat1 = new Date("01-14-2023");// DD-MM-YYYY
+// console.log(myCreatedDate.getTime());//1674412200000
+console.log(Math.floor(Date.now()/1000));
 
- let width =prompt("Enter Number  and Rectangle and width:")
- let height =prompt("Enter Number  and Rectangle and Height:")
- 
-document.write(`Rectangle Height and width  : ${ width * height}`);
+
+
+// console.log(myDateFormat.toLocaleString());//   1/14/2023, 5:30:00 AM
+// console.log(myDateFormat1.toLocaleString());//    1/14/2023, 12:00:00 AM
+
+
+
+// Timestamp 
+
+let myTimeStamp =  Date.now();
+// console.log(myTimeStamp);// 1723737359317
+
+
+
+
+
+
+
+
+
+
+
+
+
+
